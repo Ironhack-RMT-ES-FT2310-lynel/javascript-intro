c53e6cb8a5a4112006abebbc4874eee45055d5c7
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-11 12:29:43 +0200

    strings done

--- script.js
+++ script.js
@@ -84,3 +84,87 @@ console.log(var5, typeof var5)
 
 let var6 = [ "Jorge" ]
 console.log(var6, typeof var6) // Los arrays son de tipo objeto
+
+
+
+// STRINGS
+
+
+let fruit1 = "fresa";
+let fruit2 = "pera";
+let fruit3 = "tomate";
+
+
+// concatenar strings
+let receta = "La receta para un smoothie es: " + fruit1 + ", " + fruit2 + " y " + fruit3
+// console.log(receta)
+
+
+// interpolacion de strings
+// `` backticks o template literals
+let recetaMejorada = `La receta para un smoothie es ${fruit1}, ${fruit2} y ${fruit3}`
+console.log(recetaMejorada)
+
+
+
+// Indices y posiciones
+
+let simpleWord = "hola";
+//                ||||
+//                0123
+
+console.log( simpleWord.charAt(8) )
+
+// Notación de corchetes
+console.log( simpleWord[3] )
+
+
+// .length => cantidad de caracteres
+
+console.log(simpleWord.length) // 4
+
+
+// hacer un console.log del ultimo caracter de la palabra
+let lastCharacter = simpleWord[simpleWord.length - 1]
+console.log(lastCharacter)
+
+
+
+
+// indexOf => buscar la posición de una letra o palabra
+// -1 significa, no lo consiguió
+console.log(simpleWord.indexOf("x"))
+
+// includes busca si existe o no y devuelve un booleano
+
+
+
+
+// slice
+
+let longWord = "bananaramawakawakaehehsamiramira"
+
+
+let wordCopy = longWord.slice(14, 18)
+console.log(wordCopy)
+
+
+let uppercased = wordCopy.toUpperCase()
+console.log(uppercased)
+
+
+
+// capitalizar una palabra
+
+let username = "antonio";
+
+// let capitalized = username[0].toUpperCase() // "A"
+// capitalized = capitalized + username.slice(1, username.length)
+
+// let capitalized = username[0].toUpperCase() + username.slice(1, username.length)
+let capitalized = username[0].toUpperCase() + username.slice(1) // es Igual que arriba
+
+console.log(capitalized)
+
+
+
